83dc0240451e43f1f149f011ea2fa50c01e76282
Jamie <jxberc@gmail.com>
2014-03-30 14:26:23 -0400

    got rid of gap between squares: line-height

--- sketchpad/sketch.js
+++ sketchpad/sketch.js
@@ -1,12 +1,14 @@
 
-var count = 16;
+var count = 4;
 
 $(document).ready(function(){
 
-	for(var i  = 0; i < count; i++)
+
+
+	for(var i  = 0; i < count*count; i++)
 	{
 
-		$('.grid').append("<div></div>");
+		$('.grid').append("<div></div>");	
 		$('.grid > div').addClass('square');
 	}
 	
@@ -20,13 +22,32 @@ $(document).ready(function(){
 });
 
 
-
-
-
 $("button").click(function(){
 		$('div').removeClass("blue");
 
-		count = prompt("How many squares per side?",64);
+		count = prompt("How many squares per side?",4);
 
+		$(".square").remove();
+
+		
+
+
+		for(var i  = 0; i < count*count; i++)
+		{
+		$('.grid').append("<div></div>");	
+		$('.grid > div').addClass('square');
+		}
+
+		var width = 400 / count;
+		var height = 400 / count;
+		$(".square").css({"width":width ,"height":height});
+		console.log("hello");
+
+		$(".square").hover(function(){
+		$(this).addClass("blue");
+		});
+
+
+		
 
 	});
