2a37eabb91671248105443a5da8c87dc8c85372c
Jamie <jxberc@gmail.com>
2014-04-07 11:41:17 -0400

    tagging feature completed

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -3,6 +3,8 @@ Blogger::Application.routes.draw do
   resources :articles do 
     resources:comments
   end
+
+  resources :tags
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- blogger/db/schema.rb
+++ blogger/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140406232551) do
+ActiveRecord::Schema.define(version: 20140407135101) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -29,5 +29,21 @@ ActiveRecord::Schema.define(version: 20140406232551) do
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+
+  create_table "taggings", force: true do |t|
+    t.integer  "tag_id"
+    t.integer  "article_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "taggings", ["article_id"], name: "index_taggings_on_article_id"
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id"
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
