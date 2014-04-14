fdc583f15c3ac6f4fcd35ffaefb317c1dd3ab77c
Jamie <jxberc@gmail.com>
2014-04-13 20:19:21 -0400

    cleaned up ruby_advanced solutions

--- ruby_advanced/project1.rb
+++ ruby_advanced/project1.rb
@@ -22,10 +22,8 @@ def bubble_sort_by(list)
 
 	loop do
 		i, swap = 0, 0
-		while i < list.length - 1	
-
-			check = yield(list[i], list[i+1])
-			if 	check > 0 
+		while i < list.length - 1		
+			if yield(list[i], list[i+1]) > 0 
 				list[i],list[i+1] = list[i+1], list[i]
 				swap += 1
 			end
@@ -49,5 +47,6 @@ puts "Actual: #{bubble_sort([4,3,78,2,0,2])}"
 puts "Actual: #{bubble_sort_by(s_array) { |left, right| right <=> left}}"
 puts "Actual: #{bubble_sort_by(my_array) { |num1, num2| num2 <=> num1 }}"
 puts "Actual: #{bubble_sort_by([4,3,78,2,0,2]) { |num1, num2| num2 <=> num1 }}"
+print "hi"
 
 
