452eba8cb8734f47b84c8909f193222eb419adec
Jamie <jxberc@gmail.com>
2014-04-14 11:59:37 -0400

    cleaned up ruby files

--- ruby_advanced/project1.rb
+++ ruby_advanced/project1.rb
@@ -1,52 +1,43 @@
-
-
 def bubble_sort(list)
-	
-	loop do
-		i, swap = 0, 0
-		while i < list.length - 1		
-			if list[i+1] < list[i]
-				list[i],list[i+1] = list[i+1], list[i]
-				swap += 1
-			end
-			i += 1
-		end
-		break if swap == 0
-	end
-	list
+  loop do
+    i, swap = 0, 0
+    while i < list.length - 1
+      if list[i + 1] < list[i]
+        list[i], list[i + 1] = list[i + 1], list[i]
+        swap += 1
+      end
+      i += 1
+    end
+    break if swap == 0
+  end
+  list
 end
 
-
-
 def bubble_sort_by(list)
+  loop do
+    i, swap = 0, 0
+    while i < list.length - 1
+      if yield(list[i], list[i + 1]) > 0
+        list[i], list[i + 1] = list[i + 1], list[i]
+        swap += 1
+      end
+      i += 1
+    end
+    break if swap == 0
+  end
+  list
+end
 
-	loop do
-		i, swap = 0, 0
-		while i < list.length - 1		
-			if yield(list[i], list[i+1]) > 0 
-				list[i],list[i+1] = list[i+1], list[i]
-				swap += 1
-			end
-			i += 1
-		end
-		break if swap == 0
-	end
-	list
-	end
-
-####TEST CASES#####
+#### TEST CASES #####
 
 my_array = [5, 1, 4, 2, 8]
-s_array = ["hi", "hello","howdy", "how", "helium", "hey"]
+s_array = %w(hi hello howdy how helium hey)
 
 puts "Actual: #{bubble_sort(my_array)}"
 puts "Actual: #{bubble_sort(s_array)}"
-puts "Actual: #{bubble_sort([4,3,78,2,0,2])}"
+puts "Actual: #{bubble_sort([4, 3, 78, 2, 0, 2])}"
 
-#sorts descending order using block
-puts "Actual: #{bubble_sort_by(s_array) { |left, right| right <=> left}}"
+# sorts descending order using block
+puts "Actual: #{bubble_sort_by(s_array) { |left, right| right <=> left }}"
 puts "Actual: #{bubble_sort_by(my_array) { |num1, num2| num2 <=> num1 }}"
-puts "Actual: #{bubble_sort_by([4,3,78,2,0,2]) { |num1, num2| num2 <=> num1 }}"
-print "hi"
-
-
+puts "Actual: #{bubble_sort_by([4 , 3, 78, 2, 0, 2]) { |num1, num2| num2 <=> num1 }}"
--- ruby_building_blocks/project1.rb
+++ ruby_building_blocks/project1.rb
@@ -1,40 +1,27 @@
-
-
 def caesar_cipher(text, num)
-	
-	cipher = ""
-	
-	text.each_char  do |letter|  
-		#converts letter to number and shifts by num (26 is number of letters in alphabet)
-		val = letter.ord + (num % 26) 
-
-		#sets upcase letter to value  within 65 and 90, aschii values for A-Z
-		if letter =~ /[A-Z]/ && val > 90 
-				val = (val % 90) + 64  
-		
-		#sets lowercase letter to value within 97 and 122, ascii values a-z
-		elsif letter =~/[a-z]/ && val > 122	
-				val = (val % 122) + 96 
-
-		#sets puncation to ascii value without shifting
-		elsif letter =~ /\W/
-				val = letter.ord
-		end
-
-		cipher += val.chr	
+	cipher = ''
+	text.each_char  do |letter|
+	# converts letter to ascii
+	val = letter.ord + (num % 26) 
+	# sets upcase letter to value  within 65 and 90, aschii values for A-Z
+	if letter =~ /[A-Z]/ && val > 90
+		val = (val % 90) + 64
+	# sets lowercase letter to value within 97 and 122, ascii values a-z
+	elsif letter =~ /[a-z]/ && val > 122
+		val = (val % 122) + 96
+	# sets puncation to ascii value without shifting
+	elsif letter =~ /\W/
+		val = letter.ord
 	end
-	cipher 
+	cipher += val.chr
+	end
+	cipher
 end
 
-s= "What a string!"
+s = 'What a string!'
 
-#rot13 is a caesar_cipher given n = 13. If you run rot13 a second time, it will decipher message.
+# rot13 is a caesar_cipher given n = 13.
+# if you run rot13 a second time, it will decipher message.
 rot13 = caesar_cipher(s, 13)
 puts rot13
-
-#running rot13 deciphers
 puts caesar_cipher(rot13, 13)
-
-
-
-
