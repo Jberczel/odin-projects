40004878a1c3c6080e8cb312f44fb1ce41c935b6
Jamie <jxberc@gmail.com>
2014-04-15 15:02:15 -0400

    cleaned event_manager script and added readme

--- event_manager/event_manager.rb
+++ event_manager/event_manager.rb
@@ -1,8 +1,12 @@
+
+# Event Manager Tutorial from jumpstart lab
+# completed extra problems: 1)clean phone numbers 2)time targeting 3)day of the week targeting
+
 require 'csv'
 require 'sunlight/congress'
 require 'erb'
 
-Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5" # jumpstart api key
+Sunlight::Congress.api_key = 'e179a6973728c4dd3fb1204283aaccb5' # jumpstart api key
 puts 'Event Manager Initiliazed!'
 
 def clean_zipcode(zipcode)
@@ -16,9 +20,9 @@ end
 
 # creates directory of customized thank you letters
 def save_thank_you_letters(id, form_letter)
-  Dir.mkdir('output') unless Dir.exists? 'output'
+  Dir.mkdir('output') unless Dir.exist? 'output'
   filename = "output/thanks_#{id}.html"
-  File.open(filename,'w') do |file|
+  File.open(filename, 'w') do |file|
     file.puts form_letter
   end
 end
@@ -29,27 +33,28 @@ def clean_phone(phone)
   if phone.length != 10
     if phone.length == 11 && phone[0] = '1'
       phone = phone[1..-1]
+    else
+      phone = '0000000000'
     end
-    phone = '0000000000'
   end
   phone[0..2] + '-' + phone[3..5] + '-' + phone[6..9]
 end
 
 # prints list of hours with most registrants
 def peak_hours(all_hours)
-  puts "PEAK HOURS:"
-  puts "-----------"
-  peak_hours = all_hours.sort_by { |key, value| value }.reverse.first(5)
-  peak_hours. each { |hour, count| puts "hour: #{hour} count: #{count}" } 
+  puts 'PEAK HOURS:'
+  puts '-----------'
+  peak_hours = all_hours.sort_by { |k, v| v }.reverse.first(5)
+  peak_hours. each { |hour, count| puts "hour: #{hour} count: #{count}" }
   puts "\n"
 end
 
-#prints list of days with # of registrants
+# prints list of days with # of registrants
 def peak_days(days_of_week)
-  puts "PEAK DAYS"
-  puts "---------"
-  sorted_days = days_of_week.sort_by { |k,v| v}.reverse
-  sorted_days.each { |i| puts "#{i[0]}:".rjust(10,' ') + " #{i[1]}" }
+  puts 'PEAK DAYS'
+  puts '---------'
+  sorted_days = days_of_week.sort_by { |k, v| v }.reverse
+  sorted_days.each { |i| puts "#{i[0]}:".rjust(10, ' ') + " #{i[1]}" }
   puts "\n"
 end
 
@@ -65,19 +70,19 @@ days_of_week = Hash.new(0) # has to track registrants by day
 contents.each do |row|
   # pulls elements from csv file and cleans them for data processing
   id = row[0]
-	name = row[:first_name]
-	zipcode = clean_zipcode(row[:zipcode])
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
   phone = clean_phone(row[:homephone])
-  reg_date = DateTime.strptime(row[:regdate],'%m/%d/%y %H:%M')
+  reg_date = DateTime.strptime(row[:regdate], '%m/%d/%y %H:%M')
 
   # counters to keep track of registrants by hour and day
   all_hours[reg_date.hour] += 1
   days_of_week[reg_date.strftime('%A')] += 1
 
-  # pulls legislators and saves thank you letters with table of legislators
-  legislators = legislators_by_zipcode(zipcode) 
-  form_letter = erb_template.result(binding) 
-  save_thank_you_letters(id,form_letter)
+  # write seperate method for these three lines...
+  # legislators = legislators_by_zipcode(zipcode)
+  # form_letter = erb_template.result(binding)
+  # save_thank_you_letters(id,form_letter)
 end
 
 puts "\n"
---
+++ event_manager/readme.md
@@ -0,0 +1,20 @@
+##Event Manager - jumpstart labs
+My implementation of jumpstart labs Event Manager tutorial.
+
+Program parses .csv database, cleans records, creates and writes to new files, and calculates peak hour and day with the highest number of registrants in dummy data set.
+
+####The techniques practiced:
+
+File input and output
+Reading data from CSV files
+String manipulation
+Accessing Sunlight’s Congressional API through the Sunlight Congress gem
+Using ERB for templating
+
+####Requirements
+
+Ruby Environment
+Sample Data in CSV Format
+Sunlight-Congress Gem
+
+View tutorial at [jumpstart labs](http://tutorials.jumpstartlab.com/projects/eventmanager.html)
