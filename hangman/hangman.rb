74b63458445813444b4b1450595cfbb44a172f68
Jamie <jxberc@gmail.com>
2014-04-16 08:15:42 -0400

    first iteration of hangman script

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -1,41 +1,55 @@
 
-file = File.open("5desk.txt", "r")
-contents = file.read
+class Hangman
 
-valid_words = contents.split.select { |word| word.length.between?(5,12) } 
+  def initialize
+    file = File.open("5desk.txt", "r")
+    contents = file.read
+    valid_words = contents.split.select { |word| word.length.between?(5,12) }   
+    @word = valid_words[rand(valid_words.size)].downcase.split('')
+    @display = Array.new(@word.length, '_')
+    @guesses = Array.new
+    @misses = Array.new
+    @turns = 6
+  end
 
+  def play
+    puts "\nWELCOME TO HANGMAN\n"
+    result = "Sorry, you lose.  The word was #{@word.join}."
+    while @turns > 0
+      display
+      guess
+      if @display.none? { |i| i == '_' }
+        result = "#{@display.join(' ')}\nYou win!"
+        @turns = 0
+      end  
+    end
+    puts result
+  end
+  
+  def display
+    #puts @word.join
+    @display.each { |i| print "#{i}  "}
+    puts "\n"
+    puts "Misses: #{@misses.join(' ')}"
+    puts "Turns Remaining: #{@turns}"
+  end
 
-
-word = valid_words[rand(valid_words.size)].downcase.split('')
-
-
-display = Array.new(word.length, '-')
-guesses = Array.new
-misses = Array.new
-
-
-p word
-display.each { |i| print "#{i}  "}
-
-
- 
-  puts "\n"
-  puts "Misses: #{misses}"
-  puts "Turns Remaining: "
-  print "Enter guess: "
-  input = gets.chomp
-  if word.none? { |w| w == input }
-    misses << input
-  else
-    word.each_with_index do |letter, i|
-      display[i] = letter if letter == input
+  def guess
+    print "Enter guess: "
+    input = gets.chomp
+    puts "\n"
+    if @word.none? { |w| w == input }
+      @misses << input
+      @turns -= 1
+    else
+      @word.each_with_index do |letter, i|
+        @display[i] = letter if letter == input
+      end
     end
   end
 
+end
 
-
-p display
-
-
-
+game = Hangman.new
+game.play
 
