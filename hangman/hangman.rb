a91ad6375fb2bcc1c1df9e70448ebdd09b531cd0
Jamie <jxberc@gmail.com>
2014-04-16 12:25:33 -0400

    updated hangman files

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -1,50 +1,49 @@
 require 'yaml'
 
 class Hangman
-
   def initialize
-    content = File.open("5desk.txt", "r") { |file| file.read }
-    valid_words = content.split.select { |word| word.length.between?(5,12) }   
-    @word = valid_words[rand(valid_words.size)].downcase.split('')
+    content = File.open('5desk.txt', 'r') { |file| file.read } # loads dictionary file
+    valid_words = content.split.select { |word| word.length.between?(5,12) }  # filters words 5-12 letters
+    @word = valid_words[rand(valid_words.size)].downcase.split('') #selects random word
     @display = Array.new(@word.length, '_')
-    @guesses = Array.new
-    @misses = Array.new
-    @turns = 6
+    @misses = Array.new # tracks guesses that were not in word
+    @turns = 6 #turns left before game over
   end
 
+  # simulates one game of hangman
   def play
     result = "Sorry, you lose.  The word was #{@word.join}."
     while @turns > 0
-      display
-      guess
+      display # displays number of letters, misses, turns remaining
+      guess # prompts user for letters
       if @display.none? { |i| i == '_' }
         result = "#{@display.join('  ')}\nCongrats, you win!"
         @turns = 0
-      end  
+      end
     end
     puts result
   end
-  
+
+  # displays relevant info to user
   def display
-    #puts @word.join
-    @display.each { |i| print "#{i}  "}
+    @display.each { |i| print "#{i}  " }
     puts "\n"
     puts "Misses: #{@misses.join(', ')}"
     puts "Turns Remaining: #{@turns}"
   end
 
   def guess
-    print "Enter guess: "
+    print 'Enter guess: '
     input = gets.chomp
     puts "\n"
-    if input == "save"
+    if input == 'save'
       save_game
-      puts "Game has been saved.", "\n"
-    elsif @word.none? { |w| w == input }
+      puts 'Game has been saved.', "\n"
+    elsif @word.none? { |w| w == input } # add unfound letter to misses array, reduce turns
       @misses << input
       @turns -= 1
     else
-      @word.each_with_index do |letter, i|
+      @word.each_with_index do |letter, i| # add letter to display array
         @display[i] = letter if letter == input
       end
     end
@@ -65,6 +64,7 @@ def save_game
   end
 end
 
+# method used to validate y/n responses form users
 def valid_answer(q)
   input = ''
   until input == 'y' || input == 'n'
@@ -74,12 +74,13 @@ def valid_answer(q)
   input
 end
 
+# main program loop
 puts "\nWELCOME TO HANGMAN\n"
 loop do 
-  input = valid_answer("Do you want to load previously saved game (y/n)? ")
+  input = valid_answer('Do you want to load previously saved game (y/n)? ')
   puts "Thank you.  You can save game at any moment by typing 'save'."
   game = input == 'y' ? load_game : Hangman.new
   game.play
-  input2 = valid_answer("Play another game (y/n)? ")
+  input2 = valid_answer('Play another game (y/n)? ')
   break if input2 == 'n'
 end
---
+++ hangman/readme.md
@@ -0,0 +1,31 @@
+## Hangman Game
+#### Project: Serialization and Working with Files
+
+Simple command line Hangman game, where user and save and load games from yaml file.
+
+Example output:
+
+```
+WELCOME TO HANGMAN
+Do you want to load previously saved game (y/n)? n
+Thank you.  You can save game at any moment by typing 'save'.
+_  _  _  _  _  _  _
+Misses:
+Turns Remaining: 6
+Enter guess: a
+
+_  a  _  _  a  _  _
+Misses:
+Turns Remaining: 6
+Enter guess: s
+
+_  a  _  _  a  _  _
+Misses: s
+Turns Remaining: 5
+Enter guess: r
+
+_  a  _  _  a  _  _
+Misses: s, r
+Turns Remaining: 4
+Enter guess:
+```
