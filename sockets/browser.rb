abcd9cfc6172c84bca026c718bc480dc9910a4da
Jamie <jxberc@gmail.com>
2014-04-26 09:54:30 -0400

    fix GET requests

--- sockets/browser.rb
+++ sockets/browser.rb
@@ -4,15 +4,16 @@ require 'json'
  
 host = 'localhost'    
 port = 2000                          
-
 params = Hash.new{|hash, key| hash[key] = Hash.new}         
 
+# input validation: require GET or POST
 input = ''
 until input == 'GET' or input == 'POST'
   print 'What type of request do you want to submit [GET, POST]? '
   input = gets.chomp
 end
 
+# get inputs from user, save to params (hash of hash)
 if input == 'POST'
   print 'Enter name: '
   name = gets.chomp
@@ -20,21 +21,17 @@ if input == 'POST'
   email = gets.chomp
   params[:person][:name] = name
   params[:person][:email] = email
-
   body = params.to_json
-
+  # create request line to send to server
   request = 
     "POST /thanks.html HTTP/1.0\r\nContent-Length: #{params.to_json.length}\r\n\r\n#{body}"
-
 else
   request = "GET /index.html HTTP/1.0\r\n\r\n"
 end
 
-socket = TCPSocket.open(host,port)  # Connect to server
-socket.print(request)               # Send request
-response = socket.read              # Read complete response
-# Split response at first blank line into headers and body
-headers,body = response.split("\r\n\r\n", 2) 
+socket = TCPSocket.open(host,port)            # Connect to server
+socket.print(request)                         # Send request
+response = socket.read                        # Read complete response                                              
+headers,body = response.split("\r\n\r\n", 2)  # Split response at first blank line into headers and body
 print body 
-
 socket.close                       
--- sockets/server.rb
+++ sockets/server.rb
@@ -3,26 +3,28 @@ require 'json'
 
 server = TCPServer.open(2000)  
 loop {
-  Thread.start(server.accept) do |client|                       
-
+  Thread.start(server.accept) do |client|
+    # this is short-term fix for reading request from client.  Need to research best solution.                     
     request = client.read_nonblock(256)
-    request_header, request_body = request.split("\r\n\r\n", 2)
-    p request_header
-    p request_body
 
-    path = request_header.split[1][1..-1] # gets path from request header
-    if File.exists?(path)
+    request_header, request_body = request.split("\r\n\r\n", 2)   # splits request into header and body
+    path = request_header.split[1][1..-1]                         # gets path from request header
+    method = request_header.split[0]                              # gets method: GET or POST
+
+    if File.exists?(path)                                  
       response_body = File.read(path)
       client.puts "HTTP/1.1 200 OK\r\nContent-type:text/html\r\n\r\n"
-      params = {}
-      params = JSON.parse(request_body)
-      p params
-      client.puts response_body.gsub("<%= yield %>", "<li>#{params['person']['name']}</li><li>#{params['person']['email']}</li>")
+      if method == 'GET'
+        client.puts response_body
+      elsif method == 'POST'
+        params = {}
+        params = JSON.parse(request_body)
+        client.puts response_body.gsub("<%= yield %>", "<li>#{params['person']['name']}</li><li>#{params['person']['email']}</li>")
+      end
     else
       client.puts "HTTP/1.1 404 Not Found\r\n\r\n"
       client.puts "404 Error, File Could not be Found"
-    end
-   
+    end 
     client.close
   end
 
